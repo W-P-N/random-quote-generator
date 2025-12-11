0b7d4873b42a155413c9aeb95b0c6db6ddf49fb9
Parth <parthwani22@gmail.com>
2025-12-11 06:51:19 +0530

    Corrections: Add better error handling experience for user - display error function, and made efficient filter function in script.js. Improved html using semantic tags. Updated CSS code with tags.

--- script.js
+++ script.js
@@ -2,34 +2,32 @@
     // DOM References
     const topic = document.getElementById('topic');
     const getQuoteButton = document.getElementById('get-quote-button');
-    const output = document.getElementById('output');
+    const output = document.getElementsByTagName('output')[0];
 
     // Event Listener
     getQuoteButton.addEventListener('click', getRandomQuote);
 
     // Functions
-    async function getRandomQuote() {
+    async function getRandomQuote(e) {
+        e.preventDefault();
         output.replaceChildren('');
         const topicSelected = topic.value;
-        const quote = await searchQuote(topicSelected);
-        if(!quote) {
-            displayResult('Unable to get quote. Please try again later!');
-            return;
-        };
-        displayResult(quote);
+        try {
+            const quote = await searchQuote(topicSelected);
+            displayResult(quote);
+        } catch (error) {
+            displayError('Unable to get Quote');
+        }
         return;
     };
 
     async function searchQuote(topic) {
-        try {
-            const data = await fetchData();
-            const listOfQuotes = data.filter(quote => quote['topic'].map(quoteTopic => quoteTopic.toLowerCase()).includes(topic));
-            const randomQuotePick = listOfQuotes[Math.floor(Math.random() * (listOfQuotes.length + 1))];
-            return randomQuotePick;
-        } catch (error) {
-            console.error('Error: ', error);
-            return;
-        };
+        const data = await fetchData();
+        const t = topic.toLowerCase();
+        const listOfQuotes = data.filter(quote => quote['topic'].some(qt => qt.toLowerCase() === t));
+        const randomQuotePick = listOfQuotes[Math.floor(Math.random() * listOfQuotes.length)];
+        return randomQuotePick;
+        
     };
 
     async function fetchData() {
@@ -43,18 +41,33 @@
     };
 
     function displayResult(quote) {
-        console.log(quote)
         try {
-            const quoteElement = document.createElement('h3');
-            quoteElement.innerHTML = `<em>"${quote.quote}"</em`;
-            const authorElement = document.createElement('p');
-            authorElement.innerHTML = `<strong>- ${quote['author']}</strong>`;
+            const quoteElement = document.createElement('blockquote');
+            const emElement = document.createElement('em');
+            emElement.textContent = `"${quote.quote}"`;
+            quoteElement.appendChild(emElement);
+            const authorElement = document.createElement('figcaption');
+            const strongElement = document.createElement('strong');
+            strongElement.textContent = `- ${quote['author']}`;
+            authorElement.appendChild(strongElement);
             output.appendChild(quoteElement);
             output.appendChild(authorElement);
             return;
         } catch (error) {
             console.error('Error', error);
+            displayError(error);
             return;
-        }
+        };
     };
+
+    function displayError(error) {
+        output.replaceChildren();
+        output.style.backgroundColor = 'red';
+        output.style.color = 'white';
+        const textElement = document.createElement('h4');
+        const strongElement = document.createElement('strong');
+        strongElement.textContent = error;
+        textElement.appendChild(strongElement);
+        output.appendChild(textElement);
+    }
 }) ();
